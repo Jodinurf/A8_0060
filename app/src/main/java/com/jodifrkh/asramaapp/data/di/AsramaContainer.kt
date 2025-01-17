7772084e152bc3b5b225c145590b7535c1fa1fc3
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-17 23:23:41 +0700

    defined connection of kamar with baseUrl to defined the API endpoint on AsramaContainer

--- app/src/main/java/com/jodifrkh/asramaapp/data/di/AsramaContainer.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/data/di/AsramaContainer.kt
@@ -2,10 +2,13 @@ package com.jodifrkh.asramaapp.data.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.jodifrkh.asramaapp.data.repository.BangunanRepository
+import com.jodifrkh.asramaapp.data.repository.KamarRepository
 import com.jodifrkh.asramaapp.data.repository.MahasiswaRepository
 import com.jodifrkh.asramaapp.data.repository.NetworkBangunanRepository
+import com.jodifrkh.asramaapp.data.repository.NetworkKamarRepository
 import com.jodifrkh.asramaapp.data.repository.NetworkMahasiswaRepository
 import com.jodifrkh.asramaapp.data.service.BangunanService
+import com.jodifrkh.asramaapp.data.service.KamarService
 import com.jodifrkh.asramaapp.data.service.MahasiswaService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -14,6 +17,7 @@ import retrofit2.Retrofit
 interface AppContainer {
     val mahasiswaRepository : MahasiswaRepository
     val bangunanRepository : BangunanRepository
+    val kamarRepository : KamarRepository
 }
 
 class AsramaContainer : AppContainer {
@@ -33,10 +37,17 @@ class AsramaContainer : AppContainer {
         retrofit.create(BangunanService :: class.java)
     }
 
+    private val kamarService : KamarService by lazy {
+        retrofit.create(KamarService :: class.java)
+    }
+
     override val mahasiswaRepository : MahasiswaRepository by lazy {
         NetworkMahasiswaRepository(mahasiswaService)
     }
     override val bangunanRepository: BangunanRepository by lazy {
         NetworkBangunanRepository(bangunanService)
+    }
+    override val kamarRepository: KamarRepository by lazy {
+        NetworkKamarRepository(kamarService)
     }
 }
