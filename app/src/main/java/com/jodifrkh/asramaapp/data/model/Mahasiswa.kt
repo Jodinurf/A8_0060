6a9bf847a3033bf3b279329cafd639584ac5895c
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-17 11:51:35 +0700

    deklarasi class Mahasiswa berisi atribut dari table mahasiswa

---
+++ app/src/main/java/com/jodifrkh/asramaapp/data/model/Mahasiswa.kt
@@ -0,0 +1,33 @@
+package com.jodifrkh.asramaapp.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Mahasiswa (
+    @SerialName("id_mahasiswa")
+    val idMhs : Int,
+    @SerialName("nomor_identitas")
+    val nim : String,
+    @SerialName("nama_mahasiswa")
+    val nama : String,
+    val email : String,
+    @SerialName("nomor_telepon")
+    val noHp : String,
+
+    @SerialName("id_kamar")
+    val idKamar : Int
+)
+
+@Serializable
+data class MahasiswaResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Mahasiswa>
+)
+@Serializable
+data class MahasiswaDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Mahasiswa
+)
