aa859ef82ba7c7b464b83027c160a5340ad411a2
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-17 11:55:57 +0700

    create AsramaContainer derived from AppContainer. Defined the baseUrl and the path's endpoint

---
+++ app/src/main/java/com/jodifrkh/asramaapp/data/di/AsramaContainer.kt
@@ -0,0 +1,31 @@
+package com.jodifrkh.asramaapp.data.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.jodifrkh.asramaapp.data.repository.MahasiswaRepository
+import com.jodifrkh.asramaapp.data.repository.NetworkMahasiswaRepository
+import com.jodifrkh.asramaapp.data.service.MahasiswaService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val mahasiswaRepository : MahasiswaRepository
+}
+
+class AsramaContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2:3000/api/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+
+    private val mahasiswaService : MahasiswaService by lazy {
+        retrofit.create(MahasiswaService :: class.java)
+    }
+
+    override val mahasiswaRepository : MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
