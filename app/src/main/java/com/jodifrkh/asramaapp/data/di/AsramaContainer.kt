b753904efb54730dad2fae6ba595495ede6c06b5
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-19 13:03:49 +0700

    adding connection of pembayaran into container

--- app/src/main/java/com/jodifrkh/asramaapp/data/di/AsramaContainer.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/data/di/AsramaContainer.kt
@@ -7,9 +7,12 @@ import com.jodifrkh.asramaapp.data.repository.MahasiswaRepository
 import com.jodifrkh.asramaapp.data.repository.NetworkBangunanRepository
 import com.jodifrkh.asramaapp.data.repository.NetworkKamarRepository
 import com.jodifrkh.asramaapp.data.repository.NetworkMahasiswaRepository
+import com.jodifrkh.asramaapp.data.repository.NetworkPembayaranRepository
+import com.jodifrkh.asramaapp.data.repository.PembayaranRepository
 import com.jodifrkh.asramaapp.data.service.BangunanService
 import com.jodifrkh.asramaapp.data.service.KamarService
 import com.jodifrkh.asramaapp.data.service.MahasiswaService
+import com.jodifrkh.asramaapp.data.service.PembayaranService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
@@ -18,6 +21,7 @@ interface AppContainer {
     val mahasiswaRepository : MahasiswaRepository
     val bangunanRepository : BangunanRepository
     val kamarRepository : KamarRepository
+    val pembayaranRepository : PembayaranRepository
 }
 
 class AsramaContainer : AppContainer {
@@ -41,6 +45,10 @@ class AsramaContainer : AppContainer {
         retrofit.create(KamarService :: class.java)
     }
 
+    private val pembayaranService : PembayaranService by lazy {
+        retrofit.create(PembayaranService :: class.java)
+    }
+
     override val mahasiswaRepository : MahasiswaRepository by lazy {
         NetworkMahasiswaRepository(mahasiswaService)
     }
@@ -49,5 +57,9 @@ class AsramaContainer : AppContainer {
     }
     override val kamarRepository: KamarRepository by lazy {
         NetworkKamarRepository(kamarService)
+    }
+
+    override val pembayaranRepository : PembayaranRepository by lazy {
+        NetworkPembayaranRepository(pembayaranService)
     }
 }
