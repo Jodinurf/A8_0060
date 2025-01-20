ab321db29c79627f4d448b861bb1ef5f30651347
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-20 13:47:25 +0700

    adding initializer of insert, detail, and update view model for bangunan

--- app/src/main/java/com/jodifrkh/asramaapp/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/viewModel/PenyediaViewModel.kt
@@ -1,11 +1,15 @@
 package com.jodifrkh.asramaapp.ui.viewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.jodifrkh.asramaapp.AsramaApplication
+import com.jodifrkh.asramaapp.ui.viewModel.bangunan.DetailBgnViewModel
 import com.jodifrkh.asramaapp.ui.viewModel.bangunan.HomeBgnViewModel
+import com.jodifrkh.asramaapp.ui.viewModel.bangunan.InsertBgnViewModel
+import com.jodifrkh.asramaapp.ui.viewModel.bangunan.UpdateBgnViewModel
 import com.jodifrkh.asramaapp.ui.viewModel.kamar.HomeKmrViewModel
 import com.jodifrkh.asramaapp.ui.viewModel.mahasiswa.HomeMhsViewModel
 import com.jodifrkh.asramaapp.ui.viewModel.pembayaranSewa.HomePSViewModel
@@ -23,6 +27,21 @@ object PenyediaViewModel {
         }
         initializer {
             HomePSViewModel(AsramaApplication().container.pembayaranRepository)
+        }
+        initializer {
+            InsertBgnViewModel(AsramaApplication().container.bangunanRepository)
+        }
+        initializer {
+            DetailBgnViewModel(
+                createSavedStateHandle(),
+                AsramaApplication().container.bangunanRepository
+            )
+        }
+        initializer {
+            UpdateBgnViewModel(
+                createSavedStateHandle(),
+                AsramaApplication().container.bangunanRepository
+            )
         }
     }
 }
