a2ff4c63463796962d5fb7fa69b3bacf327081b8
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-20 21:48:47 +0700

    adjust the home bgn view for object calling namaBgn in dropdown

--- app/src/main/java/com/jodifrkh/asramaapp/ui/viewModel/bangunan/HomeBgnViewModel.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/viewModel/bangunan/HomeBgnViewModel.kt
@@ -1,49 +1,56 @@
 package com.jodifrkh.asramaapp.ui.viewModel.bangunan
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jodifrkh.asramaapp.data.model.Bangunan
 import com.jodifrkh.asramaapp.data.repository.BangunanRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import okio.IOException
 import retrofit2.HttpException
 
-sealed class HomeUiState{
+sealed class HomeUiState {
     data class Success(val bangunan: List<Bangunan>) : HomeUiState()
-    object Error: HomeUiState()
-    object Loading: HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
 }
 
-class HomeBgnViewModel (private val bgn: BangunanRepository): ViewModel(){
-    var bgnUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
-        private set
+
+class HomeBgnViewModel(private val repository: BangunanRepository) : ViewModel() {
+
+    private val _bgnUIState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    val bgnUIState: StateFlow<HomeUiState> get() = _bgnUIState
+
     init {
         getBgn()
     }
-    fun getBgn(){
+
+    fun getBgn() {
         viewModelScope.launch {
-            bgnUIState = HomeUiState.Loading
-            bgnUIState = try {
-                HomeUiState.Success(bgn.getBangunan().data)
-            } catch (e: IOException){
+            _bgnUIState.value = HomeUiState.Loading
+            _bgnUIState.value = try {
+                val bangunanList = repository.getBangunan().data
+                HomeUiState.Success(bangunanList)
+            } catch (e: IOException) {
                 HomeUiState.Error
-            } catch (e: HttpException){
+            } catch (e: HttpException) {
                 HomeUiState.Error
             }
         }
     }
+
     fun deleteBgn(idBgn: Int) {
-        viewModelScope.launch{
+        viewModelScope.launch {
             try {
-                bgn.deleteBangunan(idBgn)
-            } catch (e: IOException){
-                HomeUiState.Error
-            } catch (e: HttpException){
-                HomeUiState.Error
+                repository.deleteBangunan(idBgn)
+                getBgn()
+            } catch (e: IOException) {
+                _bgnUIState.value = HomeUiState.Error
+            } catch (e: HttpException) {
+                _bgnUIState.value = HomeUiState.Error
             }
         }
     }
 }
+
