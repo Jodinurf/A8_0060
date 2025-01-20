8965402b098592e032fd50f27aafe91af559bccf
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-20 13:46:51 +0700

    update page for bangunan

---
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/view/bangunan/UpdateBgnView.kt
@@ -0,0 +1,94 @@
+package com.jodifrkh.asramaapp.ui.view.bangunan
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jodifrkh.asramaapp.R
+import com.jodifrkh.asramaapp.navigation.DestinasiUpdateBgn
+import com.jodifrkh.asramaapp.ui.viewModel.PenyediaViewModel
+import com.jodifrkh.asramaapp.ui.viewModel.bangunan.UpdateBgnViewModel
+import com.jodifrkh.asramaapp.ui.widget.CustomTopAppBar
+import kotlinx.coroutines.delay
+
+@Composable
+fun UpdateBgnScreen(
+    onClickBack: () -> Unit,
+    onNavigate: () -> Unit,
+    viewModel: UpdateBgnViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val uiState = viewModel.updateBgnUiState
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            snackbarHostState.showSnackbar(message)
+            viewModel.resetSnackBarBgnMessage()
+        }
+    }
+
+    Scaffold (
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiUpdateBgn.titleRes,
+                canNavigateBack = true,
+                onBackClick = onClickBack,
+                refreshImageRes = R.drawable.icon_building,
+            )
+        },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            AnimatedVisibility(visible = true) {
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFFF5F5F5),
+                        contentColor = MaterialTheme.colorScheme.onBackground
+                    )
+                ) {
+                    InsertBodyBgn(
+                        uiState = uiState,
+                        formTitle = "Form ${DestinasiUpdateBgn.titleRes}",
+                        onValueChange = { updatedBgnEvent ->
+                            viewModel.updateInsertBgnState(updatedBgnEvent)
+                        },
+                        onClick = {
+                            val isSaved = viewModel.updateBgn()
+                            if (isSaved) {
+                                delay(1000)
+                                onNavigate()
+                            }
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
