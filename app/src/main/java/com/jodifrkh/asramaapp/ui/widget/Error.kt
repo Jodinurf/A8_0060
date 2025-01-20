d3d3db6c5db415e9a566ff999ea09bfc6bb3e49a
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-21 03:13:39 +0700

    change icon into image

--- app/src/main/java/com/jodifrkh/asramaapp/ui/widget/Error.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/widget/Error.kt
@@ -1,5 +1,6 @@
 package com.jodifrkh.asramaapp.ui.widget
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -26,11 +26,9 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
+        Image(
             painter = painterResource(R.drawable.ic_connection_error),
             contentDescription = null,
-            tint = Color(0xFFFF6F61),
-            modifier = Modifier.size(48.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
