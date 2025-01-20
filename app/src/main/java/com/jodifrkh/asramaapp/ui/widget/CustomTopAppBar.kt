d004d6149b0fe79d222667b6f18ee714b6c40f4e
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-20 13:43:04 +0700

    updated CustomTopAppBar

--- app/src/main/java/com/jodifrkh/asramaapp/ui/widget/CustomTopAppBar.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/widget/CustomTopAppBar.kt
@@ -1,49 +1,84 @@
 package com.jodifrkh.asramaapp.ui.widget
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jodifrkh.asramaapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopAppBar(
     title: String,
-    canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior? = null,
-    navigateUp: () -> Unit = { },
+    canNavigateBack: Boolean,
+    onBackClick: () -> Unit = { },
     onRefresh: () -> Unit = { },
+    refreshImageRes: Int? = null
 ) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
     CenterAlignedTopAppBar(
-        title = { Text(title) },
-        actions = {
-            Icon(
-                imageVector = Icons.Default.Refresh,
-                contentDescription = "Refresh Icon",
-                modifier = Modifier.clickable {
-                    onRefresh()
-                }
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onPrimary
+                ),
+                textAlign = TextAlign.Center,
             )
         },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior, navigationIcon = {
+        modifier = modifier
+            .fillMaxWidth(),
+        actions = {
+            if (refreshImageRes != null) {
+                Image(
+                    painter = painterResource(id = refreshImageRes),
+                    contentDescription = "Refresh Image",
+                    modifier = Modifier
+                        .padding(end = 14.dp)
+                        .size(40.dp)
+                        .clickable { onRefresh() }
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
             if (canNavigateBack) {
-                IconButton( onClick = navigateUp ) {
+                IconButton(onClick = onBackClick) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Back Arrow"
+                        contentDescription = "Navigate Back",
+                        tint = Color.White
                     )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = colorResource(R.color.primary),
+            titleContentColor = Color.White,
+        ),
+        windowInsets = WindowInsets(0) // Menghilangkan padding dari sistem
     )
 }
