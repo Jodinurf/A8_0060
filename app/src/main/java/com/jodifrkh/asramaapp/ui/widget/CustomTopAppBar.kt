4fcba4281aa3731cb4383ab0c589f478cd25b028
KhodamIblis <jodinurfarkhani04@gmail.com>
2025-01-26 07:22:41 +0700

    updated custom app bar by adding dropdown to navigate into each's page

--- app/src/main/java/com/jodifrkh/asramaapp/ui/widget/CustomTopAppBar.kt
+++ app/src/main/java/com/jodifrkh/asramaapp/ui/widget/CustomTopAppBar.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -17,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -24,6 +23,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.mutableStateOf
 import com.jodifrkh.asramaapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,9 +39,11 @@ fun CustomTopAppBar(
     canNavigateBack: Boolean,
     onBackClick: () -> Unit = { },
     onRefresh: () -> Unit = { },
-    refreshImageRes: Int? = null
+    onDropdownClick: (String) -> Unit = {},
+    refreshImageRes: Int? = null,
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var expandedMenu = remember { mutableStateOf(false) }
 
     CenterAlignedTopAppBar(
         title = {
@@ -62,6 +69,7 @@ fun CustomTopAppBar(
                         .clickable { onRefresh() }
                 )
             }
+
         },
         scrollBehavior = scrollBehavior,
         navigationIcon = {
@@ -71,6 +79,54 @@ fun CustomTopAppBar(
                         imageVector = Icons.Filled.ArrowBack,
                         contentDescription = "Navigate Back",
                         tint = Color.White
+                    )
+                }
+            }
+            else {
+                IconButton(onClick = { expandedMenu.value = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "Menu",
+                        tint = Color.White
+                    )
+                }
+                DropdownMenu(
+                    expanded = expandedMenu.value,
+                    onDismissRequest = { expandedMenu.value = false }
+                ) {
+                    DropdownMenuItem(
+                        onClick = {
+                            onDropdownClick("Bangunan")
+                            expandedMenu.value = false
+                        },
+                        text = {
+                            Text("Bangunan")
+                        })
+                    DropdownMenuItem(
+                        onClick = {
+                            onDropdownClick("Kamar")
+                            expandedMenu.value = false
+                        },
+                        text = {
+                            Text("Kamar")
+                        }
+                    )
+                    DropdownMenuItem(
+                        onClick = {
+                            expandedMenu.value = false
+                            onDropdownClick("Mahasiswa")
+                        },
+                        text = {
+                            Text("Mahasiswa")
+                        }
+                    )
+                    DropdownMenuItem(onClick = {
+                        expandedMenu.value = false
+                        onDropdownClick("Pembayaran Sewa")
+                    },
+                        text = {
+                            Text("Pembayaran Sewa")
+                        }
                     )
                 }
             }
